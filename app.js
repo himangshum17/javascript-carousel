5328ddd4b94c48288c6295728d5df78299fc3270
Himangshu Mondal <mailtoherock@gmail.com>
2020-08-31 22:45:25 +0530

    added javascript functionality

--- app.js
+++ app.js
@@ -1,7 +1,50 @@
+let carousel = document.querySelector(".carousel");
+let carouselItems = [...carousel.children];
+let prevBtn = document.querySelector(".prev");
+let nextBtn = document.querySelector(".next");
+let current = 0;
 
+// removing active class form carouselitems
+function removeActive(){
+    for (let i = 0; i < carouselItems.length; i++) {
+        carouselItems[i].classList.remove("active");
+    }
+}
 
+// showing first item of the carousel
+function showFirstitem(){
+    removeActive();
+    carouselItems[0].classList.add("active");
+}
 
+showFirstitem();
 
+// showing previous carousel items function
+function previousCarousel() {
+    removeActive();
+    carouselItems[current - 1].classList.add("active");
+    current--;
+};
 
+// showing next carousel items function
+function nextCarousel() {
+    removeActive();
+    carouselItems[current + 1].classList.add("active");
+    current++;
+}
 
+// event listner on prevous button
+prevBtn.addEventListener("click", function (){
+       if( current === 0 ){
+          current = carouselItems.length;
+       }
+       previousCarousel();
+});
 
+// event listner on next button
+nextBtn.addEventListener("click", function (){
+    if( current === carouselItems.length - 1 ){
+       current =  -1;
+    }
+    nextCarousel();
+});
